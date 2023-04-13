41c2b3cea1b554f2ceab24dc316514999bd12c5f
Muhamad Arwin Wijaya <54936971+marwinwijaya@users.noreply.github.com>
2023-04-13 16:14:44 +0700

    Initial

---
+++ search.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+      <title>Serch result</title>
+      <meta charset="utf-8">
+      
+      <meta name = "format-detection" content = "telephone=no">
+      
+      <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+      
+      <link href='//fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+      
+      
+      <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="search/search.css">
+	  	<script src="js/jquery.js"></script>
+		<script src="js/jquery-migrate-1.1.1.js"></script>
+		<script src="js/jquery.easing.1.3.js"></script>
+		<script src="js/superfish.js"></script>
+		<script src="js/jquery.mobilemenu.js"></script>
+		<script src="js/jquery.ui.totop.js"></script>
+		<script src="js/script.js"></script>
+      <script src="search/search.js"></script>
+      <!--[if (gt IE 9)|!(IE)]><!-->
+    <script src="js/jquery.mobile.customized.min.js"></script>
+      <!--<![endif]-->
+      <!--[if lt IE 8]>
+        <div style=' clear: both; text-align:center; position: relative;'>
+          <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+            <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today.">
+          </a>
+        </div>
+      <![endif]-->
+      <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <link rel="stylesheet" media="screen" href="css/ie.css">
+      <![endif]-->
+</head>
+
+<body>
+	<!--======================== header ============================-->
+	<header>
+	
+		<div class="header_top_box">
+			<div class="container">
+				<div class="row">
+					<div class="grid_12">
+						<ul class="top_box_list1">
+							<li><a href="#">Terms</a></li>
+							<li><a href="#">Privacy Policy</a></li>
+							<li><a href="#">Legal Agreement</a></li>
+						</ul>
+						
+						<ul class="top_box_list2">
+							<li>
+								24/7 Support:
+								<h6>+1 959 603 6035</h6>
+							</li>
+							<li class="mail"><a href="#">mail@demolink.org</a></li>
+						</ul>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="nav_border">
+			<div class="container">
+				<div class="row">
+					<div class="grid_12">
+						<!--======================== logo ============================-->
+						<h1>
+							<a href="index.html"><img src="images/logo.png" alt=""></a>
+						</h1>
+						<form id="search" action="search.php" method="GET" accept-charset="utf-8" class="search_form">
+		                      <input type="text" name="s">
+		                      <a onclick="document.getElementById('search').submit()" href="#">
+		                      	<span class="fa-search"></span>
+		                      </a>
+		                </form>
+		                
+		                <ul class="header_soclist">
+		                	<li><a href="#"><span class="fa-google-plus"></span></a></li>
+		                	<li><a href="#"><span class="fa-twitter"></span></a></li>
+		                	<li><a href="#"><span class="fa-facebook"></span></a></li>
+		                	<li><a href="#"><span class="fa-pinterest"></span></a></li>
+		                	<li><a href="#"><span class="fa-linkedin"></span></a></li>
+		                	<li><a href="#"><span class="fa-rss"></span></a></li>
+		                </ul>
+		                
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="grid_12">
+						<!--======================== menu ============================-->
+						<nav>
+							<ul class="sf-menu clearfix">
+								<li><a href="index.html">About</a>
+									<ul>
+										<li><a href="#">Vestibulum iaculis</a></li>
+										<li><a href="#">Fusce euismod conuat</a>
+											<ul>
+												<li><a href="#">Pellentesque sed</a></li>
+												<li><a href="#">Aliquam congue ferm</a></li>
+												<li><a href="#">Mauris accum</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Pellentesque</a></li>
+									</ul>
+								</li>
+								<li><a href="index-1.html">Our Animals</a></li>
+								<li><a href="index-2.html">Our Meats</a></li>
+								<li><a href="index-3.html">Pricing/Ordering</a></li>
+								<li><a href="index-4.html">Recipes</a></li>
+								<li><a href="index-5.html">Contacts</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+<!--==============================Content=================================-->
+
+<div id="content">
+  <div class="container">
+    <div class="row">
+        <div class="grid_12 ">
+            <h3 class="serch_title">Search result:</h3>
+            <div id="search-results"></div>
+        </div>
+    </div>
+  </div>
+  
+</div>
+<!--==============================footer=================================-->
+<footer>
+		<div class="container">
+			
+			<div class="row">
+				<div class="grid_4 m_22">
+					<div class="central_box">
+						<h4>About us</h4>
+						<ul class="footer_menu">
+							<li>
+								<a href="#">About</a>
+							</li>
+							<li>
+								<a href="#">Our Animals</a>
+							</li>
+							<li>
+								<a href="#">Our Meats</a>
+							</li>
+							<li>
+								<a href="#">Pricing/Ordering</a>
+							</li>
+							<li>
+								<a href="#">Recipes</a>
+							</li>
+							<li>
+								<a href="#">Contacts</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="grid_4 m_22">
+					<div class="central_box">
+						<h4>How to Buy</h4>
+					
+						<ul class="footer_list">
+							<li><a href="#">Pre-Order/Pick-up</a></li>
+							<li><a href="#">On Farm Store</a></li>
+							<li><a href="#">Restaurants/Retail</a></li>
+							<li><a href="#">Home Delivery</a></li>
+							<li><a href="#">Wholesale Program</a></li>
+							<li><a href="#">Gift Certificates</a></li>
+							<li><a href="#">Articles &amp; Reviews</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="grid_4 m_22">
+					<div class="central_box">
+						<h4>Get it touch</h4>
+						<ul class="footer_list">
+							<li>8901 Marmora Road</li>
+							<li>Glasgow, DO4 89GR.</li>
+							<li><span>Freephone:</span>  +1 800 559 6580</li>
+							<li><span>Telephone:</span>   +1 959 603 6035</li>
+							<li><span>FAX:</span>            + 1 504 889 9898</li>
+						</ul>
+						<ul class="footer_list">
+							<li>
+								E-mail: <a href="#">mail@demolink.org</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="grid_12">
+					<div class="copyright"><span class="site_name">Sandman’s Farm Fresh Meats.</span> &copy; <span id="copyright-year"></span> <a href="index-6.html">Privacy policy</a></div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
+
